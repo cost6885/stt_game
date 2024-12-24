f6286578e2ff0a90dea8b2b30d4f0d334e5b88fa
cost6885 <132336835+cost6885@users.noreply.github.com>
2024-12-24 17:45:27 +0900

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -271,27 +271,27 @@ scoreForm.addEventListener('submit', (e) => {
     whisperScoreDisplay.innerText = totalScore.toFixed(2);
 });
 
-// 난이도 계산
+// 난이도 계산 함수
 function calculateDifficulty() {
-    if (totalScore > 80) {
-        return '쉬움';
-    } else if (totalScore > 60) {
-        return '보통';
+    if (totalScore < 50) {
+        return "쉬움";
+    } else if (totalScore < 80) {
+        return "보통";
     } else {
-        return '어려움';
+        return "어려움";
     }
 }
 
-// 재시도 버튼
+// 재시작 버튼 클릭 시
 retryBtn.addEventListener('click', () => {
-    totalScore = 0;
     currentRound = 1;
+    totalScore = 0;
     showPage(landingPage);
 });
 
-// 결과 페이지에서 재시도 버튼
+// 결과 페이지에서 재시작 버튼 클릭 시
 retryBtnResults.addEventListener('click', () => {
-    totalScore = 0;
     currentRound = 1;
+    totalScore = 0;
     showPage(landingPage);
 });
